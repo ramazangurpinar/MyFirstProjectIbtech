1a185258214bd210f66bb130e48526f522a8d62d
Ramazan Gürpınar <ramazan.gurpinar.1997@hotmail.com>
2023-02-17 15:39:46 +0300

    activity binding

--- app/src/main/java/ramazan/gurpinar/DetailsActivity.kt
+++ app/src/main/java/ramazan/gurpinar/DetailsActivity.kt
@@ -2,14 +2,16 @@ package ramazan.gurpinar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ramazan.gurpinar.databinding.ActivityDetailsBinding
 import ramazan.gurpinar.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
