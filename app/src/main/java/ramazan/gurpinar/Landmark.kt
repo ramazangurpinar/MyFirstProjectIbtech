b2448a80e88177dd582ddb4d97dcf5f1bfa9bc49
Ramazan Gürpınar <ramazan.gurpinar.1997@hotmail.com>
2023-02-17 16:21:24 +0300

    tıklandığı zaman details activity e bilgi gönderme

--- app/src/main/java/ramazan/gurpinar/Landmark.kt
+++ app/src/main/java/ramazan/gurpinar/Landmark.kt
@@ -1,5 +1,6 @@
 package ramazan.gurpinar
 
-class Landmark(val name: String, val country: String, val image: Int) {
+import java.io.Serializable
+class Landmark(val name: String, val country: String, val image: Int) : Serializable {
 
 }
--- app/src/main/java/ramazan/gurpinar/LandmarkAdapter.kt
+++ app/src/main/java/ramazan/gurpinar/LandmarkAdapter.kt
@@ -1,5 +1,6 @@
 package ramazan.gurpinar
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -23,5 +24,10 @@ class LandmarkAdapter(val landmarkList: ArrayList<Landmark>): RecyclerView.Adapt
     override fun onBindViewHolder(holder: LandmarkHolder, position: Int) {
         holder.binding.recyclerViewNameText.text = landmarkList[position].name
         holder.binding.recyclerViewCountryText.text = landmarkList[position].country
+        holder.itemView.setOnClickListener{
+            val intent = Intent(holder.itemView.context,DetailsActivity::class.java)
+            intent.putExtra("landmark",landmarkList[position])
+            holder.itemView.context.startActivity(intent)
+        }
     }
 }
