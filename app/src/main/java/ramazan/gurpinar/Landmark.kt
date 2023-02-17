024f105d27ce8a627655ef7188af301cf0f4b13a
Ramazan Gürpınar <ramazan.gurpinar.1997@hotmail.com>
2023-02-17 15:50:31 +0300

    landmark ekleme

---
+++ app/src/main/java/ramazan/gurpinar/Landmark.kt
@@ -0,0 +1,5 @@
+package ramazan.gurpinar
+
+class Landmark(val name: String, val country: String, val image: Int) {
+
+}
--- app/src/main/java/ramazan/gurpinar/MainActivity.kt
+++ app/src/main/java/ramazan/gurpinar/MainActivity.kt
@@ -7,10 +7,25 @@ import ramazan.gurpinar.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var landmarkList: ArrayList<Landmark>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        landmarkList = ArrayList<Landmark>()
+
+        // landmark name , country name, landmark image
+        val pisa = Landmark("Pisa Kulesi","Italya",R.drawable.pisa)
+        val eyfel = Landmark("Eyfel Kulesi","Fransa",R.drawable.eyfel)
+        val londonBridge = Landmark("London Bridge","UK",R.drawable.londonbridge)
+        val tajmahal = Landmark("Taj Mahal","Hindistan",R.drawable.tajmahal)
+
+        landmarkList.add(pisa)
+        landmarkList.add(eyfel)
+        landmarkList.add(londonBridge)
+        landmarkList.add(tajmahal)
     }
 }
